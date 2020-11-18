d59bad923147e652d2ab2db87720df78d9babec7
Alexander Pöppl <a.poeppl@rivent.de>
2020-11-18 17:22:33 +0100

    fix contact form

--- scripts.js
+++ scripts.js
@@ -9,23 +9,17 @@ function debugBibTexElement(bibtexEntry) {
 
 function sendMail() {
     var form = document.getElementById("contact-form");
-    var receiverEmail = document.getElementById("contact-response-name");
-    var receiverName = document.getElementById("contact-response-email");
+    var receiverName = document.getElementById("contact-response-name");
+    var receiverEmail = document.getElementById("contact-response-email");
     var text = document.getElementById("contact-text");
-    Email.send({
-        SecureToken : "A0628A1131790617A5C6BE76185E3AEA1A75E4C75A8B86581D4B489DF5A00C38B98E266C4F3091BF4CE4F40CBACD6DE1",
-        To : 'alex-nax@web.de',
-        From : 'nax@live.de',
-        Subject : `Contact from ${receiverName} (${receiverEmail})`,
-        Body : text
-    }).then(
-      message => alert(text)
+    var status = Email.send({
+        SecureToken : "bafe2b53-bf29-486d-a5d5-7634e37f9720",
+        To : "alex-nax@web.de",
+        From : "alex-nax@web.de",
+        Subject : `Contact from ${receiverName.value} (${receiverEmail.value})`,
+        Body : text.value
+    });
+    status.then(
+        message => alert(message)
     );
 }
-
-/*
-var bibtexEntry = function(bibtexEntry) {
-    console.log("a");
-    var entireThing = bibtexentry.toString();
-    console.log(entireThing);
-}*/
