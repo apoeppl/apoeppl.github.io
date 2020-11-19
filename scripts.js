fbf50ce8e3a83ec0cb9f61990d15a7d879f42b45
Alexander Pöppl <a.poeppl@rivent.de>
2020-11-19 16:56:49 +0100

    fix message form.

--- scripts.js
+++ scripts.js
@@ -7,19 +7,41 @@ function debugBibTexElement(bibtexEntry) {
     console.log(bibtexEntry.innerHtml);
 }
 
+function reactToMessage(message) {
+    if (message === "OK") {
+        var receiverName = document.getElementById("contact-response-name");
+        var receiverEmail = document.getElementById("contact-response-email");
+        var text = document.getElementById("contact-text");
+        text.value = "";
+        receiverName.value = "";
+        receiverEmail.value = "";
+        alert("Message has been sent!");
+    } else {
+        alert("Message could not be sent.");
+    }
+
+}
+
 function sendMail() {
     var form = document.getElementById("contact-form");
     var receiverName = document.getElementById("contact-response-name");
     var receiverEmail = document.getElementById("contact-response-email");
     var text = document.getElementById("contact-text");
-    var status = Email.send({
+    Email.send({
         SecureToken : "bafe2b53-bf29-486d-a5d5-7634e37f9720",
-        To : "alex-nax@web.de",
+        To : "nax@live.nl",
         From : "alex-nax@web.de",
         Subject : `Contact from ${receiverName.value} (${receiverEmail.value})`,
         Body : text.value
-    });
-    status.then(
-        message => alert(message)
+    }).then(
+        (message) => {
+            reactToMessage(message);
+        }
+    ).catch(
+        (error) => {
+            console.log(error);
+            alert("Unable to send Message!");
+        }
     );
+    return false;
 }
